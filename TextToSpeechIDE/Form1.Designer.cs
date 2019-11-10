0393391fccba0fb4ed1a470df1e4e6d9088c1dff
Александр <@->
2019-11-10 19:15:18 +0300

    Добавьте файлы проекта.

---
+++ TextToSpeechIDE/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TextToSpeechIDE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WB = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // WB
+            // 
+            this.WB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WB.Location = new System.Drawing.Point(0, 0);
+            this.WB.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WB.Name = "WB";
+            this.WB.Size = new System.Drawing.Size(800, 450);
+            this.WB.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.WB);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser WB;
+    }
+}
+
